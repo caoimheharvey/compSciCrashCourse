5cea666fba41a1b1328b02cb38df8a175b12310e
caoimheharvey <caoimhe.e.harvey@gmail.com>
2021-11-13 10:13:54 +0800

    Adding second basics assignment

---
+++ PythonExercises/YourSecondPythonApp.py
@@ -0,0 +1,17 @@
+"""
+Academic System
+----------------
+- This system should open up to a main menu that allows the user to select from different options:
+    1. Class Management
+        - Create a new class
+        - Get the number of enrolled students in a class
+        - Delete a class (only if the number of enrolled students is less than 2)
+        - Get the average grade of all students in the class
+    2. Student Management
+        - Create a new student account
+        - Delete a student (only if they are not enrolled in any classes)
+        - Add a grade for a student in a class
+        - Enroll in a class
+        - Display average grade for each class
+
+"""
