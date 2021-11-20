491a78c04b99abddf2d8bab10b2e1035c25aea8d
Alexandre Wang <darksailor@hotmail.fr>
2021-11-20 15:59:57 +0800

    Changing to new data structure

--- PythonExercises/YourSecondPythonApp.py
+++ PythonExercises/YourSecondPythonApp.py
@@ -16,8 +16,13 @@ Academic System
 
 """
 
-class_info = {}
-unenrolled_student_info = {}
+# key class, value student id
+class_info = {'Math': [1, 2, 3],
+              'CS': [1, 3],
+              'Art': [4]}
+
+# key student id, value dict storing name, class grades
+student_info = {1: {'name': 'Alex', 'Math': 80, 'CS': 30}, 2: {'name': 'Caoimhe', 'Math': 50}, 3: {'name': 'Chou', 'CS': 90, 'Math': 80}, 4: {'name': 'Hans', 'Art': 10}}
 
 
 # Initial view
@@ -62,7 +67,7 @@ def class_mgmt_prompt():
             delete_class(class_info)
         elif class_mgmt_user_option == 4:
             print("option 4")
-            class_average_grade(class_info)
+            class_average_grade(class_info, student_info)
         else:
             print("Returning to home page")
             return True
@@ -70,7 +75,7 @@ def class_mgmt_prompt():
 
 def create_new_class(class_info):
     new_class_key = input("Input the name of the new class \n")
-    new_class_temp = {new_class_key: {}}
+    new_class_temp = {new_class_key: []}
     class_info.update(new_class_temp)
     print(class_info)
     print("New class " + new_class_key + " created")
@@ -84,27 +89,31 @@ def get_student_number(class_info):
         print("This class does not exist")
 
 
+# deleting class should delete student grade in class as well
 def delete_class(class_info):
     class_to_delete = input("Which class do you want to delete? \n")
     if class_to_delete not in class_info:
         print("This class doesn't exist")
     elif len(class_info[class_to_delete]) < 2:
+        for i in class_info[class_to_delete]:
+            student_info[i].pop(class_to_delete)
         class_info.pop(class_to_delete)
-        print("Class " + class_to_delete + " successfully deleted")
+        print(class_to_delete + " class successfully deleted")
         print(class_info)
+        print(student_info)
     else:
         print("Can't delete a class comprised of 2 students or more")
         print(class_info)
 
 
-def class_average_grade(class_info):
+def class_average_grade(class_info, student_info):
     class_grade_input = input("Which class do you want to get average grade from? \n")
     if class_grade_input not in class_info:
         print("This class doesn't exist")
     elif not class_info[class_grade_input]:
         print("No student enrolled in class")
     else:
-        class_average = sum([class_info[class_grade_input][x] for x in class_info[class_grade_input]]) / len(
+        class_average = sum([student_info[x][class_grade_input] for x in class_info[class_grade_input]]) / len(
             class_info[class_grade_input])
         print("Average of class " + class_grade_input + " is equal to: " + str(class_average))
 
@@ -122,13 +131,13 @@ def student_mgmt_prompt():
         if student_mgmt_user_option not in [1, 2, 3, 4, 5, 6]:
             print("Please input 1 to 6 to choose between options")
         elif student_mgmt_user_option == 1:
-            create_new_student(unenrolled_student_info)
+            create_new_student(student_info)
         elif student_mgmt_user_option == 2:
-            delete_student(unenrolled_student_info)
+            delete_student(student_info)
         elif student_mgmt_user_option == 3:
-            add_student_grade(class_info)
+            change_student_grade(class_info)
         elif student_mgmt_user_option == 4:
-            enroll_student_to_class(class_info, unenrolled_student_info)
+            enroll_student_to_class(class_info, student_info)
         elif student_mgmt_user_option == 5:
             print("option 5")
 
@@ -136,55 +145,62 @@ def student_mgmt_prompt():
             print("Returning to home page")
             return True
 
-def create_new_student(unenrolled_student_info):
-    new_student_key = input("Input the name of the new student \n")
-    if new_student_key in unenrolled_student_info:
-        print("Student " + new_student_key + " exists already")
-    else:
-        unenrolled_student_info.update({new_student_key: None})
-        print(unenrolled_student_info)
-        print("New student " + new_student_key + " account created")
 
-def delete_student(unenrolled_student_info):
-    student_to_delete = input("Which student account do you want to delete? \n")
-    if student_to_delete in unenrolled_student_info:
-        unenrolled_student_info.pop(student_to_delete)
-        print("Account of student " + student_to_delete + " successfully deleted")
-        print(unenrolled_student_info)
-    else:
-        print("Student doesn't exist")
+def create_new_student(student_info):
+    new_student_name = input("Input the name of the new student \n")
+    new_student_key = sorted(student_info.keys())[-1] + 1
+    student_info.update({new_student_key: {'name': new_student_name}})
+    print(student_info)
+    print("New student " + new_student_name + " account created")
 
-def add_student_grade(class_info):
-    student_grade_class_prompt = input("In which class is the student currently enrolled in? \n")
-    if student_grade_class_prompt in class_info:
-        student_grade_name_prompt = input("What is the name of the student? \n")
-        if student_grade_name_prompt in class_info[student_grade_class_prompt]:
-            add_student_grade_prompt = input("Please input the student grade \n")
-            if add_student_grade_prompt.isnumeric() and int(add_student_grade_prompt) >= 0 and int(
-                    add_student_grade_prompt) < 101:
-                class_info[student_grade_class_prompt][student_grade_name_prompt] = int(add_student_grade_prompt)
-                print(class_info)
+
+def delete_student(student_info):
+    print(student_info)
+    student_to_delete = input("Please input the ID of the student account you wish to delete \n")
+    if int(student_to_delete) in student_info:
+        student_info.pop(int(student_to_delete))
+        print("Account of student ID " + student_to_delete + " successfully deleted")
+        print(student_info)
+    else:
+        print("ID is incorrect or student doesn't exist")
+
+
+def change_student_grade(class_info):
+    student_grade_prompt = input("Please input the ID of the student account you want to change grade for \n")
+    if int(student_grade_prompt) in student_info:
+        student_grade_class_prompt = input("For which class are you change grade for? \n")
+        if student_grade_class_prompt in student_info[int(student_grade_prompt)]:
+            grade_prompt = input("Please input the grade for the selected class \n")
+            if grade_prompt.isnumeric() and int(grade_prompt) >= 0 and int(grade_prompt) < 101:
+                student_info[int(student_grade_prompt)][student_grade_class_prompt] = grade_prompt
+                print("Grade changed successfully")
+                print(student_info)
             else:
                 print("Grade should be within 0 and 100")
         else:
-            print("Student doesn't exist in class")
+            print("Class is incorrect or doesn't exist")
     else:
-        print("Class doesn't exist")
+        print("ID is incorrect or student doesn't exist")
 
-def enroll_student_to_class(class_info, unenrolled_student_info):
-    print(unenrolled_student_info)
-    student_to_enroll_prompt = input("Which student would you like to enroll? \n")
-    if student_to_enroll_prompt in unenrolled_student_info:
+
+def enroll_student_to_class(class_info, student_info):
+    print(student_info)
+    student_to_enroll_prompt = input("Please input the ID of the student you'd like to enroll \n")
+    if int(student_to_enroll_prompt) in student_info:
         class_to_enroll_prompt = input("In which class would you like to enroll the student to? \n")
         if class_to_enroll_prompt in class_info:
-            class_info[class_to_enroll_prompt].update({student_to_enroll_prompt: None})
-            unenrolled_student_info.pop(student_to_enroll_prompt)
-            print(class_info)
-            print(unenrolled_student_info)
+            if int(student_to_enroll_prompt) in class_info[class_to_enroll_prompt]:
+                print("Student is already enrolled in this class")
+            else:
+                class_info[class_to_enroll_prompt].append(int(student_to_enroll_prompt))
+                student_info[int(student_to_enroll_prompt)].update({class_to_enroll_prompt: None})
+                print(class_info)
+                print(student_info)
         else:
             print("Class doesn't exist")
     else:
         print("Student doesn't exist or is already enrolled")
+
 
 login_prompt()
 # class_mgmt_prompt()
