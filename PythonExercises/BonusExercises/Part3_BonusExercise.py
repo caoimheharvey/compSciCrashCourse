cb3a577f25c594916f65056a1d319d2128e0f3bd
caoimheharvey <caoimhe.e.harvey@gmail.com>
2021-12-10 17:36:02 +0800

    Updating solutions for bonus exercises

--- PythonExercises/BonusExercises/Part3_BonusExercise.py
+++ PythonExercises/BonusExercises/Part3_BonusExercise.py
@@ -18,3 +18,25 @@ Source - Facebook Interview Question
  # Write a program to read in the data files from disk, it must then print the names
  # of only the bipedal dinosaurs from fastest to slowest. Do not print any other information.
 """
+
+g = 9.8
+
+dinosaurs = {}
+dataset1 = open("dino_race_dataset1.csv")
+dataset2 = open("dino_race_dataset2.csv")
+
+for line in dataset2:
+    name, stride, stance = line.split(",")
+    if "bipedal" in stance:
+        dinosaurs[name] = float(stride)
+
+for line in dataset1:
+    name, leg, diet = line.split(",")
+    if name in dinosaurs.keys():
+        stride = dinosaurs[name]
+        dinosaurs[name] = ((stride / float(leg) - 1) * (float(leg) * g) ** 0.5)
+
+# sorted key param?
+# https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
+dinosaurs = dict(sorted(dinosaurs.items(), key=lambda item: item[1], reverse=True))
+for k in dinosaurs: print(k)
