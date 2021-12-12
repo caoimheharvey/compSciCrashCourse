92b0855d17c55ce35b1305a562afc9eb1750f37c
caoimheharvey <caoimhe.e.harvey@gmail.com>
2021-12-12 12:50:30 +0800

    Adding more practice exercises

---
+++ PythonExercises/BonusExercises/dict_review.py
@@ -0,0 +1,26 @@
+"""
+1. Sort a dictionary by values
+"""
+
+
+
+"""
+2. Sort a dictionary by key
+"""
+
+
+"""
+3. Merge two dictionaries
+"""
+
+
+
+"""
+4. Find the key with the maximum unique values
+"""
+
+
+
+"""
+5. 
+"""
