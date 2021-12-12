5c821346e7411906759eab40a628fa8011d80da1
Alexandre Wang <darksailor@hotmail.fr>
2021-12-12 12:13:58 +0800

    adding unfinished part 3

--- PythonExercises/BonusExercises/Part3_BonusExercise.py
+++ PythonExercises/BonusExercises/Part3_BonusExercise.py
@@ -18,3 +18,46 @@ Source - Facebook Interview Question
  # Write a program to read in the data files from disk, it must then print the names
  # of only the bipedal dinosaurs from fastest to slowest. Do not print any other information.
 """
+
+# ds1 = open('C:\\Users\\darks\\Documents\\git\\compSciCrashCourse\\PythonExercises\\files\\dino_race_dataset1.csv', 'r')
+# read_ds1 = ds1.read()
+# print(read_ds1)
+# ds1.close()
+#
+# ds2 = open('C:\\Users\\darks\\Documents\\git\\compSciCrashCourse\\PythonExercises\\files\\dino_race_dataset2.csv', 'r')
+# read_ds2 = ds2.read()
+# print(read_ds2)
+# ds2.close()
+
+dictdino = {}
+
+with open('C:\\Users\\darks\\Documents\\git\\compSciCrashCourse\\PythonExercises\\files\\dino_race_dataset1.csv', 'r') as file:
+    counter = 0
+    for line in file:
+        NAME, LEG_LENGTH, DIET = line.split(',')
+        DIET = DIET.replace('\n', '')
+        if counter != 0:
+            dictdino[NAME] = {'leg_length': float(LEG_LENGTH), 'diet': DIET}
+        counter += 1
+
+# can also be
+# with open('..\\files\\csvfile') as file
+with open('C:\\Users\\darks\\Documents\\git\\compSciCrashCourse\\PythonExercises\\files\\dino_race_dataset2.csv', 'r') as file:
+    counter = 0
+    for line in file:
+        NAME, STRIDE_LENGTH, STANCE = line.split(',')
+        STANCE = STANCE.replace('\n', '')
+        if counter != 0:
+            dictdino[NAME].update({'stride_length': float(STRIDE_LENGTH), 'stance': STANCE})
+        counter += 1
+
+print(dictdino)
+
+for dino in dictdino:
+    if 'stride_length' in dictdino[dino] and 'leg_length' in dictdino[dino]:
+        SPEED = ((dictdino[dino]['stride_length'] / dictdino[dino]['leg_length']) - 1)\
+                * (dictdino[dino]['leg_length'] * 9.8) ** 0.5
+        dictdino[dino].update({'speed': SPEED})
+
+
+print(dictdino)
