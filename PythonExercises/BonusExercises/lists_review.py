084bd469658af91ab0ec41707f0ddcef96446fd4
caoimheharvey <caoimhe.e.harvey@gmail.com>
2021-12-13 12:44:19 +0800

    Adding bonus question solns

--- PythonExercises/BonusExercises/lists_review.py
+++ PythonExercises/BonusExercises/lists_review.py
@@ -7,37 +7,55 @@ etc.
 """
 e1_list = [20, 30, 40, 10, 50, 30, 59, 43, 50, 43, 34, 40, 54, 23, 12, 43, 60]
 # write code here
-
+for i in range(5):
+    start = e1_list[i]
+    end = e1_list[-(i+1)]
+    e1_list[i] = end
+    e1_list[-(i+1)] = start
+print(e1_list)
 
 """
 2. Reverse the list in exercise 1
 """
 # write code here
-
+print(list(reversed(e1_list)))
 
 """
 3. Output only the largest two numbers in the original list from exercise 1
 """
 # write code here
-
+sorted_list = sorted(e1_list, reverse=True)
+print(sorted_list[:2])
 
 """
 4. Output only the smallest two numbers in the original list from exercise 1
 """
 # write code here
+print(sorted_list[-2:])
 
 
 """
 5. Count the occurrence of a user inputted value in the list from exercise 1
 """
+import random
 # write code here
+try:
+    user_input = int(input("Enter a number: "))
+except ValueError:
+    print("Integer not added...")
+    user_input = random.randint(0, 100)
 
+print(e1_list.count(user_input))
 
 """
 6. Count all unique values in the list from exercise 1
 """
 # write code here
-
+unique = []
+for item in e1_list:
+    if e1_list.count(item) == 0:
+        unique.append(item)
+print(unique)
 
 """
 7. Check if a list contains three consecutive common numbers and output which they are.
@@ -47,4 +65,10 @@ The result should be 1 and 5 as three consecutive common numbers
 e7_list = [4, 5, 6, 7, 7, 7, 5, 4, 4, 3, 3, 3, 3, 12, 21, 12, 2, 2, 12, 12, 12, 32, 32, 43, 54, 5, 4, 44, 4, 44, 45, 6,
            7, 55, 55, 55]
 # write code here
+consecutive = []
+for i in range(len(e7_list) - 2):
+    subset = e7_list[i:i+3]
+    if subset[0] == subset[1] == subset[2]:
+        consecutive.append(subset[0])
 
+print(consecutive)
--- PythonExercises/BonusExercises/strings_review.py
+++ PythonExercises/BonusExercises/strings_review.py
@@ -2,36 +2,58 @@
 Update the first half of the below string to be uppercase and the second half to be lower case.
 """
 half_n_half = "This is a string that is half upper case and half lower case."
-
+half_length = round(len(half_n_half)/2)
+res = half_n_half[:half_length].upper() + half_n_half[half_length:].lower()
+print(res)
 
 """
 Reverse the above string (interview question by Microsoft)
 """
+print(res[::-1])
+# or
+print(half_n_half[::-1])
 
 
 """
 Find the least frequently occuring character in the above string
 """
+char_occurrence = {}
+for c in half_n_half.lower():
+    if c not in char_occurrence.keys():
+        char_occurrence[c] = 1
+    else:
+        char_occurrence[c] += 1
 
+res = min(char_occurrence, key=char_occurrence.get)
+print(res)
 
 
 """
 Check if the following string contains any non-alphanumeric characters
 """
 s = "Do I...have non alpha numeric characters?"
-
+print(s.isalnum())
 
 """
 Remove the non-alpha numeric characters from the above string
 """
+i = 0
+print(s)
+while i < len(s):
+    if not s[i].isalnum():
+        s = s[:i] + s[i+1:]
+    else:
+        i += 1
 
-
+print(s)
 
 """
 Rotate a string.
 Given a string, rotate it so that the first 3 characters appear in the end
 """
 string = 'hellomynameiselderprice'
+string = string[3:] + string[:3]
+print(string)
 
 
 """
@@ -39,13 +61,23 @@ How many times do you need to rotate the above original string to obtain the res
 """
 
 expected_result = 'derpricehellomynameisel'
+counter = 0
+while string != expected_result:
+    string = string[3:] + string[:3]
+    counter += 1
 
+print(counter)
 
 """
 Given a list of strings, remove the letter x from all of them.
 """
 list_of_strings = ['itsx', 'bexginxing', 'to', 'lookx', 'xa', 'lot', 'lixke', 'chrxistxmxaxs']
 
+for i in range(len(list_of_strings)):
+    string = list_of_strings[i]
+    list_of_strings[i] = ''.join([s for s in string if s != 'x'])
+
+print(list_of_strings)
 
 """
 Check if a substring is present in a string
@@ -54,3 +86,4 @@ Check if a substring is present in a string
 substr = 'needle in haystack'
 main_string = 'its like finding a needle in a haystack'
 
+print(substr in main_string)
